075e003f540769c23512cccd332ef772758b453c
sasaplus1 <sasaplus1@gmail.com>
2022-03-19 00:44:28 +0900

    add class to inline code block

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 const util = require('util');
 
+const markdownIt = require('markdown-it');
+
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
+const mi = markdownIt();
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
@@ -11,6 +15,19 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('src/index.css');
   // wait for esbuild
   eleventyConfig.setWatchThrottleWaitTime(100);
+
+  // add class to inline code block
+  const originalCodeInline =
+    mi.renderer.rules.code_inline ||
+    function (tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options);
+    };
+  mi.renderer.rules.code_inline = function (tokens, idx, options, env, self) {
+    tokens[idx].attrPush(['class', 'language-']);
+
+    return originalCodeInline(tokens, idx, options, env, self);
+  };
+  eleventyConfig.setLibrary('md', mi);
 
   // ref: https://github.com/11ty/eleventy/issues/1526#issuecomment-731855231
   eleventyConfig.addFilter('inspect', function (content) {
